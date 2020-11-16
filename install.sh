d8d005f82098301c94bfeafde070cf0c11bfe643
hhdx <lchhdx@qq.com>
2020-11-16 14:54:36 +0800

    Fix autojump install

--- install.sh
+++ install.sh
@@ -59,6 +59,10 @@ setup_omz() {
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     # zsh-syntax-highlighting
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    # autojump
+    git clone git://github.com/wting/autojump.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autojump
+    cd ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autojump
+    ./install.py
     # powerlevel10k
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 }
